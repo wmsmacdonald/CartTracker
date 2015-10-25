8fc65629f38f0a297c16f7b20ed29f86a9c4b94a
Mike Dokken <mkdokken@macbookpro2.local>
2015-10-24 21:33:53 -0500

    Initial Commit

---
+++ CartTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CartTracker
+//
+//  Created by Mike Dokken on 10/24/15.
+//  Copyright © 2015 Mike Dokken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CartTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CartTracker
+//
+//  Created by Mike Dokken on 10/24/15.
+//  Copyright © 2015 Mike Dokken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
